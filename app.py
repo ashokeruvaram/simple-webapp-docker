cc7355bc0ad41e268679bd786d0df5da8e13b23c
ashokeruvaram <101696136+ashokeruvaram@users.noreply.github.com>
2022-12-22 12:57:22 +0530

    updated

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "Welcome deployment!"
+    return "Welcome deployment through build config!"
 
 @app.route('/how are you')
 def hello():
